d7fbdf63df41533daefd0f9878741334f20b700b
DEV <cht.houdayfa@gmail.com>
2026-05-11 13:40:56 +0300

    terraform enterprise structure

---
+++ envs/dev/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+module "nginx" {
+  source         = "../../modules/nginx"
+  container_name = "nginx-dev"
+  external_port  = 8083
+}
---
+++ envs/stage/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+module "nginx" {
+  source         = "../../modules/nginx"
+  container_name = "nginx-stage"
+  external_port  = 8084
+}
